7b391d558bed734f15f341f8073c2cc8794bc9d1
Chattox <conradlineker@gmail.com>
2023-04-08 17:44:55 +0100

    feat: camera streaming api

--- picam_backend/app.py
+++ picam_backend/app.py
@@ -1,6 +1,12 @@
 import io
 from threading import Condition
-from flask import Flask, request
+
+from picamera2 import Picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
+import libcamera
+
+from flask import Flask, Response
 from flask_cors import CORS, cross_origin
 
 class StreamingOutput(io.BufferedIOBase):
@@ -13,15 +19,33 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration(main={"size": (1280, 720)})
+video_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
+picam2.configure(video_config)
+output = StreamingOutput()
+picam2.start_recording(JpegEncoder(), FileOutput(output))
 
+def stream_img():
+    while True:
+        with output.condition:
+            output.condition.wait()
+            frame = output.frame
+        yield (b'--frame\r\n'
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
 
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+try:
+    app = Flask(__name__)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route("/stream", methods=["GET"])
-@cross_origin()
-def get_stream():
-    return "hello world"
+    @app.route("/stream", methods=["GET"])
+    @cross_origin()
+    def get_stream():
+        return Response(stream_img(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-app.run(host='0.0.0.0', threaded=True)
+    app.run(host='0.0.0.0', threaded=True)
+
+finally:
+    picam2.stop_recording()
