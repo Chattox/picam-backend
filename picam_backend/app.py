7a3b35e8e222dadf759e89fcb1093515ef33ba39
Chattox <conradlineker@gmail.com>
2023-04-08 16:54:33 +0100

    feat: StreamingOutput

--- picam_backend/app.py
+++ picam_backend/app.py
@@ -1,5 +1,19 @@
+import io
+from threading import Condition
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -9,3 +23,5 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @cross_origin()
 def get_stream():
     return "hello world"
+
+app.run(host='0.0.0.0', threaded=True)
