632b5003481b2fb1af61f449c8d421637df64bc1
Chattox <conradlineker@gmail.com>
2023-04-13 14:31:35 +0100

    feat(api): extremely basic auth

--- api/app.py
+++ api/app.py
@@ -1,5 +1,10 @@
-from flask import Flask, request
+from os import environ
+from dotenv import load_dotenv
+from flask import Flask, Response, request
 from flask_cors import CORS, cross_origin
+
+load_dotenv()
+AUTH_PASSWORD = environ.get("AUTH_PASSWORD")
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -8,6 +13,9 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route("/", methods=["GET"])
 @cross_origin()
 def get_picam():
-    return "hello world"
+    if request.args.get("auth") == AUTH_PASSWORD:
+        return "hello world"
+    else:
+        return Response("Unauthorised", status=401)
 
 app.run(host='0.0.0.0', threaded=True)
