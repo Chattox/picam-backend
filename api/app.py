5e5ae77e751d26657996a18fef67cc37b6721e8d
Chattox <conradlineker@gmail.com>
2023-04-14 17:32:18 +0100

    wip: threaded obj detection

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask import Flask, Response, request
 from flask_cors import CORS, cross_origin
 from picam.Picam import Picam
+import time
 
 load_dotenv()
 AUTH_PASSWORD = environ.get("AUTH_PASSWORD")
@@ -17,6 +18,7 @@ camera = Picam()
 @cross_origin()
 def get_picam():
     if request.args.get("auth") == AUTH_PASSWORD:
+        camera.start_obj_det()
         return Response(camera.livestream(), mimetype='multipart/x-mixed-replace; boundary=frame', status=200)
     else:
         return Response("Unauthorised", status=401)
--- picam/Picam.py
+++ picam/Picam.py
@@ -1,14 +1,16 @@
 import io
-from threading import Condition
+import threading
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 import libcamera
 
+import time
+
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
         self.frame = None
-        self.condition = Condition()
+        self.condition = threading.Condition()
     
     def write(self, buf):
         with self.condition:
@@ -22,12 +24,24 @@ class Picam:
         self.video_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
         self.camera.configure(self.video_config)
         self.output = StreamingOutput()
-        self.camera.start_recording(JpegEncoder(), FileOutput(self.output))      
+        self.camera.start_recording(JpegEncoder(), FileOutput(self.output))  
+        self.thread_abort = False # This will come in handy later
 
     def livestream(self):
-        while True:
+        while not self.thread_abort:
             with self.output.condition:
                 self.output.condition.wait()
                 frame = self.output.frame
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+    def obj_detect(self):
+        while not self.thread_abort:
+            print('Hello, world')
+            arr = self.camera.capture_array()
+            print(arr)
+            time.sleep(3)
+
+    def start_obj_det(self):
+        obj_det_thread = threading.Thread(target=self.obj_detect)
+        obj_det_thread.start()
