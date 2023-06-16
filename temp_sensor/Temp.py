340341f56555bd34d8d96822ae9b6aef7b8c1e6f
Chattox <conradlineker@gmail.com>
2023-06-16 23:26:20 +0100

    feat: min/max temperature tracking

--- temp_sensor/Temp.py
+++ temp_sensor/Temp.py
@@ -4,8 +4,23 @@ from w1thermsensor import W1ThermSensor
 class TempSensor:
     def __init__(self):
         self.sensor = W1ThermSensor()
+        self.min_temp = 999
+        self.min_time = time.time() * 1000
+        self.max_temp = 999
+        self.max_time = time.time() * 1000
 
     def get_temp(self):
         temp_c = self.sensor.get_temperature()
-        res = { 'temp': temp_c }
+        if self.min_temp == 999:
+            self.min_temp = temp_c
+        if self.max_temp == 999:
+            self.max_temp = temp_c
+        if temp_c < self.min_temp:
+            self.min_temp = temp_c
+            self.min_time = time.time() * 1000
+        if temp_c > self.max_temp:
+            self.max_temp = temp_c
+            self.max_time = time.time() * 1000
+
+        res = { 'temp': temp_c, 'minTemp': {'temp': self.min_temp, 'time': self.min_time}, 'maxTemp': {'temp': self.max_temp, 'time': self.max_time}, 'time': time.time() * 1000 }
         return res
